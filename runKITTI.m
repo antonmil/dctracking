f73007f982d29a2fb74d31dad10c9bd0f23232ca
amilan <devnull@localhost>
2014-12-04 12:01:57 +1030

    KITTI

--- runKITTI.m
+++ runKITTI.m
@@ -1,11 +1,12 @@
 %%
 allscen=[500:520 550:578];
 allscen=[550:578];
+allscen=1550:1578;
 for s=allscen
     try
         scenario=s;
 %         [metrics2d, metrics3d, allens, stateInfo]=dcTracker(scenario);
-        [metrics2d, metrics3d, allens, stateInfo]=swDCTracker(s,'config/1127Ka-1/0014.ini')
+        [metrics2d, metrics3d, allens, stateInfo]=swDCTracker(s,'config/1202Kc-2/0001.ini')
 %         startPT=stateInfo;
         save(sprintf('/home/amilan/research/projects/dctracking/out/s%04d.mat',s),'stateInfo','metrics*');
     catch err
@@ -14,11 +15,12 @@ for s=allscen
 end
 
 %%
+addpath('/home/amilan/storage/databases/KITTI/tracking/devkit_tracking/matlab');
 for s=allscen
     try
         load(sprintf('/home/amilan/research/projects/dctracking/out/s%04d.mat',s));
         tracklets=convertToKITTI(stateInfo);
-        writeLabels(tracklets,'./out',s);
+        writeLabels(tracklets,'./out',s-allscen(1));
     catch err
         fprintf('%s\n',err.identifier);
     end
