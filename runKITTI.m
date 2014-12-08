6e145ce51b0bc22831dbb96039665bfb49476ef3
amilan <devnull@localhost>
2014-12-08 16:58:46 +1030

    KITTI

--- runKITTI.m
+++ runKITTI.m
@@ -1,8 +1,9 @@
 %%
 allscen=[500:520 550:578];
 allscen=[550:578];
-allscen=1550:1578;
-setting='1202Kc-2';
+allscen=1750:1778;
+setting='1204Kb-2';
+tarclass='pedestrian';
 outdir=sprintf('results/%s',setting);
 if ~exist(outdir,'dir'), mkdir(outdir); end
 
@@ -13,7 +14,7 @@ for s=allscen
 %         [metrics2d, metrics3d, allens, stateInfo]=dcTracker(scenario);
         conffile=sprintf('config/%s/0001.ini',setting);
         outfile=fullfile(outdir,sprintf('s%04d.mat',s));
-        [metrics2d, metrics3d, allens, stateInfo]=swDCTracker(s,conffile)
+        [metrics2d, metrics3d, allens, stateInfo]=swDCTracker(s,conffile);
 %         startPT=stateInfo;
         save(outfile,'stateInfo','metrics*');
     catch err
@@ -28,7 +29,7 @@ for s=allscen
     try
         outfile=fullfile(outdir,sprintf('s%04d.mat',s));
         load(outfile);
-        tracklets=convertToKITTI(stateInfo);
+        tracklets=convertToKITTI(stateInfo,tarclass);
         writeLabels(tracklets,outdir,s-allscen(1));
     catch err
         fprintf('%s\n',err.identifier);
